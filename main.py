76658490cd67818fc5a6c9f008ecb901b670818d
billy k <wkkraft@gmail.com>
2016-01-24 17:23:52 -0600

    Update main.py
    added lxml

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import urllib
+from lxml import etree
 
-website = urllib.urlopen("http://google.com")
-print website.info()
+website = urllib.urlopen("http://news.google.com")
+page = website.read()
+html = etree.HTML(page)
+pretty = etree.tostring(html, pretty_print=True, method="html")
+# print pretty
