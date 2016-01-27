7386dd36dfc7dacd82f32d1724c5918130f9a5aa
billy k <wkkraft@gmail.com>
2016-01-27 17:19:17 -0600

    object oriented
    starting URL is now passed from command line

--- main.py
+++ main.py
@@ -1,39 +1,50 @@
-# gather 1000 unique urls
+# gather 1000 unique urls and print error messages
 
 from lxml.html import parse
+import sys
 
-urls = []
-dups = []
+class crawler:
 
-start = 'http://www.google.com/'
-urls.append(start)
+	def __init__(self, start_url):
+		self.urls = []
+		self.duplicates = []
+		self.urls.append(start_url)
 
-def get_page():
-	site = urls.pop()
-	dups.append(site)
-	try:
-		page = parse(site).getroot()
-		return page
-	except IOError as e:
-		print e
-		return None
+	def get_page(self):
+		site = self.urls.pop()
+		self.duplicates.append(site)
+		try:
+			page = parse(site).getroot()
+			return page
+		except IOError as e:
+			print e
+			return None	
 
-def scrape_links(page):
-	page.make_links_absolute()
-	for link in page.iterlinks():
-		if link[1] == 'href':
-			if link[2] not in dups and link[2] not in urls:
-				urls.append(link[2])
-			elif link[2] in urls:
-				dups.append(link[2])
+	def scrape_links(self, page):
+		page.make_links_absolute()
+		for link in page.iterlinks():
+			if link[1] == 'href':
+				if link[2] not in self.duplicates and link[2] not in self.urls:
+					self.urls.append(link[2])
+				elif link[2] in self.urls:
+					self.duplicates.append(link[2])	
 
-while 0 < len(urls) < 1000:
-	page = get_page()
+
+if len(sys.argv) == 2:
+	start_url = sys.argv[1]
+else:
+	print 'run as $python main.py <START_URL>'
+	sys.exit()
+
+crawly = crawler(start_url)
+
+while 0 < len(crawly.urls) < 1000:
+	page = crawly.get_page()
 	if page != None:
-		scrape_links(page)
+		crawly.scrape_links(page)	
 
-print len(urls)
-if len(urls) > 1000:
+print len(crawly.urls)
+if len(crawly.urls) > 1000:
 	print 'success!'
 else:
-  print '=['
+	print 'no more links!'
