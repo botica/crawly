e0d6042bf49bf929ef4d4f71a59861afed93795c
billy k <wkkraft@gmail.com>
2016-01-26 23:55:45 -0600

    basic crawling functionality

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
-import urllib
-from lxml import etree
+# gather 1000 unique urls
 
-website = urllib.urlopen("http://news.google.com")
-page = website.read()
-html = etree.HTML(page)
-pretty = etree.tostring(html, pretty_print=True, method="html")
-# print pretty
+from lxml.html import parse
+
+urls = []
+dups = []
+
+start = 'http://www.google.com/'
+urls.append(start)
+
+def get_page():
+	site = urls.pop()
+	dups.append(site)
+	try:
+		page = parse(site).getroot()
+		return page
+	except IOError as e:
+		print e
+		return None
+
+def scrape_links(page):
+	page.make_links_absolute()
+	for link in page.iterlinks():
+		if link[1] == 'href':
+			if link[2] not in dups and link[2] not in urls:
+				urls.append(link[2])
+			elif link[2] in urls:
+				dups.append(link[2])
+
+while 0 < len(urls) < 1000:
+	page = get_page()
+	if page != None:
+		scrape_links(page)
+
+print len(urls)
+if len(urls) > 1000:
+	print 'success!'
+else:
+  print '=['
