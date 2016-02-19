35cb7c12bd003398d9f5701b4e4e726f1c4c1a95
billy k <wkkraft@gmail.com>
2016-02-19 04:54:38 -0600

    more descriptive comments and print statements
    added docstrings, more descriptive print statements, more comments

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# minimal crawler script. gathers unique 'href' attrs from <a> tags, searchs breadth first, crawls until URL_LIMIT is reached. view urls on completion optional. prints updates to console.
+
 import urllib
 from lxml.html import parse
 import sys
@@ -6,15 +8,15 @@ import time
 URL_LIMIT = 500
 
 def is_image(url):
-	'returns true if url is common image, false if not'
-	if url.endswith('.jpg') or url.endswith('.png') or url.endswith('.gif') or url.endswith('.gifv') or url.endswith('.svg') or url.endswith('.swf') or url.endswith('.bmp') or url.endswith('pdf'):
+	'''takes filename as a string. returns True if url has common image extension, False if not'''
+	if url.endswith('.jpg') or url.endswith('.png') or url.endswith('.gif') or url.endswith('.gifv') or url.endswith('.ico') or url.endswith('.svg') or url.endswith('.swf') or url.endswith('.bmp') or url.endswith('.pdf'):
 		return True
 	else:
 		return False
 
 def is_script(url):
-	'returns true if script, false if not'
-	if url.startswith('javascript') or url.startswith('mailto') or url.startswith('tel'):
+	'''takes filename as string. returns True if undesired script, False if not'''
+	if url.startswith('javascript:') or url.startswith('mailto:') or url.startswith('tel:') or url.startswith('ios-app:'):
 		return True
 	else:
 		return False
@@ -22,16 +24,18 @@ def is_script(url):
 class crawler:
 
 	def __init__(self, start_url):
+		'''initializes member variables'''
 		self.urls = []
 		self.popped = []
 		self.urls.append(start_url)
 		self.fetched = 0
 
 	def total_urls(self):
-		'returns number of total urls'
+		'''returns number of total unique urls gathered'''
 		return len(self.urls) + len(self.popped)
 
 	def get_page(self):
+		'''attempts to retrieve resource. attempts to parse resource. returns lxml.HtmlElement object or None'''
 		site = self.urls.pop()
 		self.popped.append(site)
 		try:
@@ -45,21 +49,22 @@ class crawler:
 			print e
 			return None
 		except UnicodeEncodeError:
-			print 'problem parsing site'
+			print 'format error'
 			return None
 		except UnicodeError:
-			print 'problem parsing site'
+			print 'format error'
 			return None
 
 	def scrape_links(self, page):
+		'''takes lxml.HtmlElement as page. adds unique links to url queue.'''
 		new_links = 0
 		page.make_links_absolute()
 		for link in page.iterlinks():
 			if link[1] == 'href':
 				url = link[2]
-				if url not in self.popped and url not in self.urls: # new find
-					if (not is_image(url)) and (not is_script(url)): # only want parsable documents
-						self.urls.append(url) # add it to list of urls to be crawled
+				if url not in self.popped and url not in self.urls:       # new find
+					if (not is_image(url)) and (not is_script(url)):  # only want parsable documents
+						self.urls.append(url) 			  # add it to list of urls to be crawled
 						new_links += 1
 						if self.total_urls() == URL_LIMIT:
 							return
@@ -75,11 +80,11 @@ crawly = crawler(start_url)
 print 'scraping for ' + str(URL_LIMIT) + ' uniques urls.'
 start_time = time.time()
 
-while (0 < crawly.total_urls() < URL_LIMIT) and (len(crawly.urls) > 0):
+while (0 < crawly.total_urls() < URL_LIMIT) and (len(crawly.urls) > 0): 		  # only want URL_LIMIT urls
 	page = crawly.get_page()
 	if page != None:
 		crawly.scrape_links(page)	
-	print str(crawly.total_urls()) + ' unique links'
+	print str(crawly.total_urls()) + ' unique links gathered'
 	print str(crawly.fetched) + ' resources fetched'
 
 if crawly.total_urls() == URL_LIMIT:
@@ -90,8 +95,8 @@ if crawly.total_urls() == URL_LIMIT:
 else:
 	print 'out of links!'
 
-response = raw_input('view urls? y/n\n')
-if response == 'y' or response == 'yes':
+response = raw_input('view urls? y/n\n')						  # prompt user to print urls
+if response.upper() == 'y'.upper() or response == 'yes'.upper():
 	print_urls = crawly.urls
 	for url in crawly.popped:
 		print_urls.append(url)
